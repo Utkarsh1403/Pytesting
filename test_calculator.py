f341bb3ac68058da397af6482756d08de33ebfa5
Utkarsh1403 <utkarshsontakke123@gmail.com>
2022-03-29 15:40:09 +0530

    updated

--- test_calculator.py
+++ test_calculator.py
@@ -1,27 +1,24 @@
 import Calculator
+import pytest
+@pytest.mark.parametrize("a,b,c",[(3,2,5),(10,12,15),(2,5,8),(7,8,15)])
+def test_Add(a,b,c):
 
+    result= Calculator.addition(a,b)
+    assert c ==result
 
-def test_Add():
-    x = 10
-    y = 25
-    result= Calculator.addition(x,y)
-    assert x+y==result
+@pytest.mark.parametrize("a,b,c",[(5,2,3),(15,10,5),(25,14,15)])
+def test_sub(a,b,c):
+    result = Calculator.substraction(a,b)
+    assert c==result
 
-def test_sub():
-    x = 10
-    y = 20
-    result = Calculator.substraction(x,y)
-    assert x-y==result
+@pytest.mark.parametrize("a,b,c",[(2,3,6),(3,5,15),(7,2,15),(7,5,15)])
+def test_multi(a,b,c):
+    result = Calculator.multiply(a,b)
+    assert c == result
+@pytest.mark.parametrize("a,b,c",[(20,10,2),(15,3,5),(20,5,4),(7,5,15)])
+def test_div(a,b,c):
 
-def test_multi():
-    x = 10
-    y = 20
-    result = Calculator.multiply(x,y)
-    assert x*y == result
-def test_div():
-    x = 10
-    y = 20
-    result = Calculator.divide(x,y)
-    assert x/y== result
+    result = Calculator.divide(a,b,)
+    assert c== result
 
 
